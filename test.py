550b4cac92c07384523aa1c9e90911e33c81e96d
VapourMan <87738686+VapourMan@users.noreply.github.com>
2021-07-20 21:19:02 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,23 @@
-def add (y,x):
-    print (y+x)
-add (12,13)
+x= 13
+y=14
+z=17
+
+# add function adds two numbers x and y
+
+def add (value1,value2):
+    return value1+value2
+
+
+# print Yes if sum of x and y is greater than sum y and z else print No
+"""
+operator can be ==, <=, >=, <, >
+if(condition 1 operator condition 2):
+    task
+else:
+    task
+"""
+
+if(add(x,y)>add(y,z)):
+    print('Yes')
+else:
+    print('No')
